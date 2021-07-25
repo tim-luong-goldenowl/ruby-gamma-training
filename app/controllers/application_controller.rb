c44691a6c57cdda026e7ac5012918283a07f5b95
tim-luong-goldenowl <tim.luong.goldenowl@gmail.com>
2021-07-25 13:55:53 +0700

    Exercise object service

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,33 @@
 class ApplicationController < ActionController::Base
-  def index
-    # DemoWorker.perform_async
-    render json: {users: User.all}
+  def create_user
+    ActiveRecord::Base.transaction do
+      exsiting_deleted_user = User.only_deleted.find_by_email(user_params[:email])
+      exsiting_deleted_user.really_destroy! if exsiting_deleted_user.present?
+
+      @user = User.new(user_params)
+
+      @user.set_default_password
+      @user.set_random_username
+      @user.created_by = current_user
+      @user.add_role(:user)
+
+      if @user.save!
+        Mailer.send_confirmation_email(@user)
+        render json: {success: true}
+      end
+    end
+
+  rescue ActiveRecord::RecordInvalid
+    render json: {success: false}
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :email,
+      :name,
+      :age
+    )
   end
 end
